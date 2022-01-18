5ff0b996e5b608c8bfde37103e8d469243aaa179
Harry <Harry@gmail.com>
2022-01-18 18:54:14 +0530

    i made ujjwal folder and ujjwal.js file with class

---
+++ src/ujjwal/ujjwa.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ujjwa extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
